ad3d1ed7b4c876827e1f3058ffd0401ba1d7418a
DukulH <dukul.howlader30@gmail.com>
2022-10-01 15:03:07 +0600

    created landing page

---
+++ src/components/LandingPage/LandingPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card
+      class="d-flex justify-end  mb-6 mt-10"
+      flat
+      tile
+>
+      <v-card
+        class="headerBox pa-3 justify-center"
+        outlined
+        tile
+        color="#F1C41E"
+       
+      >
+      <v-card-text class="font-weight-bold text-center" style="font-size: 20px;">
+        <font-awesome-icon icon="fa-solid fa-house" /> Sayburgh Solutions
+      </v-card-text> 
+    </v-card>
+    </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.headerBox {
+    width: 400px;
+    margin-right: 100px;  
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
 
+library.add(faHouse)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 new Vue({
