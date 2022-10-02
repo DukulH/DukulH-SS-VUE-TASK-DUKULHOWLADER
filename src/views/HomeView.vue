4a86bfe2cc85c7e010ea3b1225ca0e566e85e552
DukulH <dukul.howlader30@gmail.com>
2022-10-02 11:51:18 +0600

    creating sidebar component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,17 @@
 <template>
- <h1>hello</h1>
+<v-app id="inspire">
+    <Sidebar />
+    <v-main style="margin:0">
+      <h1>Hello home</h1>
+    </v-main>
+</v-app>
 </template>
 
 <script>
+import Sidebar from '@/components/Sidebar/Sidebar.vue';
 
 export default {
-  name: "Home",
+    name: "Home",
+    components: { Sidebar }
 };
 </script>
